5e4b4e275a523ce64e76095abf645c62deebfdf7
twilight <kmatsix15@gmail.com>
2022-10-18 10:05:07 +0200

    js practice

---
+++ 18.10/variables.js
@@ -0,0 +1,48 @@
+
+//how to create variables//
+
+var fname="twilight";  //fname is identifier
+
+//what are key words in js//
+
+// keywords are predefined words in programming
+//.var,console.function,if , for ,while e.t.c
+
+//what are identifiers?
+ //words used to identify.
+
+ function add(a,b){
+    return a+b;
+ }
+ //a,b identifier
+ //function and return are keywwords
+ //add identifier
+
+ //what are datatypes?
+ // strings,numbers, boolean ,undefined 
+
+ //indicates type of data
+
+ //Dynamically typed =>JS , python
+ var x =10;
+ var isTrue= true
+
+ //to find out data types of a variable
+
+ console.log(typeof x);
+ var isTrue= true;
+ console.log(typeof isTrue);
+
+ //objects
+ //stores data in key:value pair
+
+ var employee= {id:1 name:"John Doe", age:35}
+ console.log(employee);
+ console.log(typeof employee)
+
+ //undefined
+ var z=undefined;
+
+ console.log(z)
+ console.log(typeof z)
+
